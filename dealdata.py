a9c86e5ce371b8190d3fad8de0244aa5479c1853
Tina <qeeing@foxmail.com>
2024-05-21 14:50:59 +0800

    每日爬取的数据放入新sheet中

--- dealdata.py
+++ dealdata.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime, timedelta
+from openpyxl import load_workbook
 
 url = "https://www.njhouse.com.cn/data/index"
 response = requests.get(url)
@@ -15,23 +16,34 @@ if response.status_code == 200:
 		secondHandHouse = tables[3] # 获取二手房住宅数据
 		
 		data_list = []
-		yesterday_time = datetime.now() + timedelta(days = -1)
-		yesterday = yesterday_time.strftime("%Y-%m-%d")
-		
 		trs = secondHandHouse.find_all('tr')
 		for tr in trs:
 			tds = tr.find_all('td')
 			if len(tds) == 5:
 				data = {
-					'日期': yesterday, # 昨日
 					'区属': tds[0].text,
 					'成交套数': tds[3].text
 				}
 				data_list.append(data)
+		
+		df = pd.DataFrame(data_list)
+		
+		yesterday_time = datetime.now() + timedelta(days = -1)
+		yesterday = yesterday_time.strftime("%Y-%m-%d")
+		sheet_name = yesterday # 昨日日期作为sheet表名
+		
+		excel_path = '南京二手房住宅成交数据.xlsx'
+		try:
+			# 非首次追加和覆盖sheet内容
+			data = load_workbook(excel_path)
+			with pd.ExcelWriter(excel_path, mode='a',
+			if_sheet_exists='replace') as writer:
+				df.to_excel(writer, sheet_name=sheet_name)
+		except FileNotFoundError:
+			# 首次创建
+			df.to_excel(excel_path, sheet_name=sheet_name)
 else:
 	print("获取网络数据出错了")
 	
-df = pd.DataFrame(data_list)
-df.to_excel('南京二手房住宅成交数据.xlsx', index=False)
 	
 	
